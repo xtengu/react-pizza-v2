d7ca69855ebac2e4545e827594e6d0b81d8aeec8
mainzonder <no7au53r@gmail.com>
2023-02-12 23:07:54 +0200

    #14

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import debounce from "lodash.debounce";
 import {SearchContext} from "../../App";
 
 import styles from "../Search/search.module.scss";
 
 const Search = () => {
-  const {searchValue, setSearchValue} = React.useContext(SearchContext);
+  const [value, setValue] = React.useState("");
+  const {setSearchValue} = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue("")
+    setValue('')
+    inputRef.current.focus();
+  };
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    []
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={styles.root}>
@@ -45,15 +66,16 @@ const Search = () => {
       </svg>
 
       <input
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder='Поиск пиццы...'
       />
 
-      {searchValue && (
+      {value && (
         <svg
-          onClick={() => setSearchValue("")}
+          onClick={onClickClear}
           className={styles.clearIcon}
           enableBackground='new 0 0 24 24'
           height='24px'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,87 +1,103 @@
-import React from "react";
-import {useSelector, useDispatch} from "react-redux";
+import React from 'react'
+import axios from 'axios'
+import { useSelector, useDispatch } from 'react-redux'
 
-import {setCategoryId} from "../redux/slices/filterSlices";
-import Sort from "../components/Sort";
-import Categories from "../components/Categories";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/PizzaBlock/Skeleton";
-import Pagination from "../components/Pagination";
-import {SearchContext} from "../App";
+import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlices'
+import Sort from '../components/Sort'
+import Categories from '../components/Categories'
+import PizzaBlock from '../components/PizzaBlock'
+import Skeleton from '../components/PizzaBlock/Skeleton'
+import Pagination from '../components/Pagination'
+import { SearchContext } from '../App'
 
 export const Home = () => {
-  const dispatch = useDispatch();
-  const {categoryId, sort} = useSelector((state) => state.filter);
+    const dispatch = useDispatch()
+    const { categoryId, sort , currentPage} = useSelector((state) => state.filter)
 
-  // https://63bd637ad660062388a3f5d4.mockapi.io/items
+    // https://63bd637ad660062388a3f5d4.mockapi.io/items
 
-  const {searchValue, _} = React.useContext(SearchContext);
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
+    const { searchValue } = React.useContext(SearchContext)
+    const [items, setItems] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+   
 
-  const [currentPage, setCurrentPage] = React.useState(1);
+    const onChangeCategory = (id) => {
+        dispatch(setCategoryId(id))
+    }
 
-  const onChangeCategory = (id) => {
-    dispatch(setCategoryId(id));
-  };
+    const onChangePage = (number) =>{
+        dispatch(setCurrentPage(number))
+    }
 
-  React.useEffect(() => {
-    setIsLoading(true);
+    React.useEffect(() => {
+        setIsLoading(true)
 
-    const sortBy = sort.sortProperty.replace("-", "");
-    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
-    const category = categoryId > 0 ? `category=${categoryId}` : "";
-    const search = searchValue ? `&search=${searchValue}` : "";
+        const sortBy = sort.sortProperty.replace('-', '')
+        const order = sort.sortProperty.includes('-') ? 'asc' : 'desc'
+        const category = categoryId > 0 ? `category=${categoryId}` : ''
+        const search = searchValue ? `&search=${searchValue}` : ''
 
-    fetch(
-      `https://63bd637ad660062388a3f5d4.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+        //   fetch(
+        //     `https://63bd637ad660062388a3f5d4.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+        //   )
+        //     .then((res) => {
+        //       return res.json();
+        //     })
+        //     .then((arr) => {
+        //       setTimeout(() => {
+        //         setItems(arr);
+        //         setIsLoading(false);
+        //       }, 1000);
+        //     });
+
+        axios
+            .get(
+                `https://63bd637ad660062388a3f5d4.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+            )
+            .then((res) => {
+                setItems(res.data)
+                setIsLoading(false)
+            })
+
+        window.scrollTo(0, 0)
+    }, [categoryId, sort.sortProperty, searchValue, currentPage])
+
+    const pizzas = items
+        // .filter((obj) => {
+        //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+        //     return true;
+        //   }
+        //   return false;
+        // })
+        .map((obj) => (
+            <PizzaBlock
+                key={obj.id}
+                {...obj}
+            />
+        ))
+
+    const skeletons = [...new Array(6)].map((_, index) => (
+        <Skeleton key={index} />
+    ))
+
+    return (
+        <div className='container'>
+            <div className='content__top'>
+                {/* {Categories() }  */}
+
+                <Categories
+                    value={categoryId}
+                    onChangeCategory={onChangeCategory}
+                />
+                <Sort />
+            </div>
+            <h2 className='content__title'>Все пиццы</h2>{' '}
+            <div className='content__items'>
+                {isLoading ? skeletons : pizzas}
+            </div>{' '}
+            <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+        </div>
     )
-      .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setTimeout(() => {
-          setItems(arr);
-          setIsLoading(false);
-        }, 1000);
-      });
-    window.scrollTo(0, 0);
-  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+}
 
-  const pizzas = items
-    // .filter((obj) => {
-    //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-    //     return true;
-    //   }
-    //   return false;
-    // })
-    .map((obj) => (
-      <PizzaBlock
-        key={obj.id}
-        {...obj}
-      />
-    ));
-
-  const skeletons = [...new Array(6)].map((_, index) => (
-    <Skeleton key={index} />
-  ));
-
-  return (
-    <div className='container'>
-      <div className='content__top'>
-        {/* {Categories() }  */}
-
-        <Categories
-          value={categoryId}
-          onChangeCategory={onChangeCategory}
-        />
-        <Sort />
-      </div>
-      <h2 className='content__title'>Все пиццы</h2>{" "}
-      <div className='content__items'>{isLoading ? skeletons : pizzas}</div>{" "}
-      <Pagination onChangePage={(number) => setCurrentPage(number)} />
-    </div>
-  );
-};
-
-export default Home;
+export default Home
